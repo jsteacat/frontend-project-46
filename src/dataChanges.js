79878b73f2bdb8c3a22519b83009aa2edebd9d51
Stepan Kormilin <ubscoder@gmail.com>
2024-12-28 20:08:27 +0300

    rework dataChanges

--- src/dataChanges.js
+++ src/dataChanges.js
@@ -1,51 +1,29 @@
-import { isEqual, isPlainObject } from 'lodash';
+import { isPlainObject, orderBy } from 'lodash';
 
-const diff = (obj1, obj2, depth = 1) => {
-  // 1: get objects keys
+const diffTree = (obj1, obj2) => {
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
+  
+  const keys = [...new Set([...keys1, ...keys2])];
+  const sortedKeys = orderBy(keys);
 
-  // 2: get all unique keys
-  // eslint-disable-next-line
-  const allKeys = Array.from(new Set([...keys1, ...keys2])).sort();
-
-  return allKeys.map((key) => {
-    if (!keys1.includes(key) && keys2.includes(key)) {
-      return {
-        key,
-        type: 'added',
-        value: obj2[key],
-      };
+  return sortedKeys.map((key) => {
+    if (!Object.hasOwn(obj2, key)) {
+      return {key, state: 'removed', value: obj1[key]};
     }
-    if (keys1.includes(key) && !keys2.includes(key)) {
-      return {
-        key,
-        type: 'deleted',
-        value: obj1[key],
-      };
-    }
-    if (!isEqual(obj1[key], obj2[key])) {
-      return {
-        key,
-        type: 'changed',
-        oldValue: obj1[key],
-        value: obj2[key],
-      };
+    if (!Object.hasOwn(obj1, key)) {
+      return {key, state: 'added', value: obj2[key]};
     }
     if (isPlainObject(obj1[key]) && isPlainObject(obj2[key])) {
-      return {
-        key,
-        type: 'nested',
-        value: diff(obj1[key], obj2[key], depth + 1),
-        depth,
-      }
+      return {key, state: 'nested', value: diffTree(obj1[key], obj2[key])};
     }
-    return {
-      key,
-      type: 'unchanged',
-      value: obj1[key],
-    };
+    if (obj1[key] !== obj2[key]) {
+      return {
+        key, state: 'updated', oldValue: obj1[key], newValue: obj2[key],
+      };
+    }
+    return {key, state: 'unchanged', value: obj1[key]};
   });
 };
 
-export default diff;
+export default diffTree;

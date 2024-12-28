3f5ab1276731daca62d2154358bee3ddb98151b9
Stepan Kormilin <ubscoder@gmail.com>
2024-12-28 20:02:24 +0300

    rework plain && stylish

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,18 +1,40 @@
-const plain = (tree) => tree.reduce((acc, prop) => {
-  switch (prop.type) {
-    case 'added':
-      return [...acc, `Property '${prop.key}' was added with value: ${prop.value}`];
-    case 'deleted':
-      return [...acc, `Property '${prop.key}' was removed`];
-    case 'changed':
-      return [...acc, `Property '${prop.key}' was updated. From ${prop.oldValue} to ${prop.value}`];
-    case 'nested':
-      return [...acc, `Property '${prop.key}' is nested. ${plain(prop.value.map((el) => ({ ...el, key: `${prop.key}.${el.key}` })))}`];
-    case 'unchanged':
-      return acc;
-    default:
-      throw new Error(`Unknown type: ${prop.type}`);
+import isObject from '../utils/isObject.js';
+
+const defineValue = (value) => {
+  if (isObject(value)) {
+    return '[complex value]';
   }
-}, []).join('\n');
+  if (typeof value === 'string') {
+    return `'${value}'`;
+  }
+  return value;
+};
+
+const plain = (tree) => {
+  const innerFunc = (node) => {
+    const {
+      key, state, value, oldValue, newValue,
+    } = node;
+    switch (node.state) {
+      case 'added':
+        return `Property '${key}' was ${state} with value: ${defineValue(value)}`;
+      case 'updated':
+        return `Property '${key}' was ${state}. From ${defineValue(oldValue)} to ${defineValue(newValue)}`;
+      case 'unchanged':
+        return [];
+      case 'removed':
+        return `Property '${key}' was ${state}`;
+      case 'nested':
+        return value.flatMap((el) => {
+          const newKey = `${key}.${el.key}`;
+          const newEl = { ...el, key: newKey };
+          return innerFunc(newEl);
+        });
+      default:
+        throw new Error(`Invalid node state - ${state}`);
+    }
+  };
+  return tree.flatMap(innerFunc).join('\n');
+};
 
 export default plain;
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,26 +1,50 @@
 import isObject from '../utils/isObject.js';
 
-const setTab = (depth) => '  '.repeat((depth * 2) - 2);
-const renderObject = (obj, depth) => {
-  if (!isObject(obj)) return obj;
-  const objectToString = Object.entries(obj).map(([key, value]) => `${setTab(depth + 1)}${key}: ${renderObject(value, depth + 1)}`);
-  return `{\n${objectToString.join('\n')}\n${setTab(depth)}}`;
+const states = {
+  added: '+ ',
+  removed: '- ',
+  unchanged: '  ',
+  nested: '  ',
+  updated: ['- ', '+ '],
 };
 
-const renderType = {
-  added: (node, depth) => `${setTab(depth)}  + ${node.key}: ${renderObject(node.value, depth + 1)}`,
-  deleted: (node, depth) => `${setTab(depth)}  - ${node.key}: ${renderObject(node.value, depth + 1)}`,
-  changed: (node, depth) => [
-    `${setTab(depth)}  - ${node.key}: ${renderObject(node.oldValue, depth)}`,
-    `${setTab(depth)}  + ${node.key}: ${renderObject(node.value, depth)}`,
-  ],
-  unchanged: (node, depth) => `${setTab(depth)}    ${node.key}: ${renderObject(node.value, depth + 1)}`,
+const defineValue = (value, depth) => {
+  const spacesCount = 4;
+  const indentSize = depth * spacesCount + spacesCount;
+  const bracketIndent = indentSize - spacesCount;
+  const blank = ' ';
+  if (!isObject(value)) {
+    return value;
+  }
+  const entries = Object.entries(value);
+  const result = entries.map(([key, val]) => `${blank.repeat(indentSize)}${key}: ${defineValue(val, depth + 1)}`);
+  return ['{', ...result, `${blank.repeat(bracketIndent)}}`].join('\n');
 };
 
-export default (tree) => {
-  const iter = (node, depth) => {
-    const dataToString = node.flatMap((elem) => renderType[elem.type](elem, depth, iter));
-    return `{\n${dataToString.join('\n')}\n${setTab(depth)}}`;
+const stylish = (tree) => {
+  const innerFunc = (node, depth) => {
+    const spacesCount = 4;
+    const leftIndent = 2;
+    const indentSize = depth * spacesCount - leftIndent;
+    const blank = ' ';
+    const bracketIndent = indentSize + leftIndent;
+    
+    const {
+      key, state, value, oldValue, newValue,
+    } = node;
+    
+    if (node.state !== 'nested' && node.state !== 'updated') {
+      return `${blank.repeat(indentSize)}${states[state]}${key}: ${defineValue(value, depth)}`;
+    }
+    if (node.state === 'updated') {
+      return `${blank.repeat(indentSize)}${states[state][0]}${key}: ${defineValue(oldValue, depth)}\n${blank.repeat(indentSize)}${states[state][1]}${key}: ${defineValue(newValue, depth)}`;
+    }
+    if (node.state === 'nested') {
+      return `${blank.repeat(indentSize)}${states[state]}${key}: {\n${node.value.map((el) => innerFunc(el, depth + 1)).join('\n')}\n${blank.repeat(bracketIndent)}}`;
+    }
+    throw new Error(`Invalid node state - ${state}`);
   };
-  return iter(tree, 1);
+  return `{\n${tree.map((el) => innerFunc(el, 1)).join('\n')}\n}`;
 };
+
+export default stylish;

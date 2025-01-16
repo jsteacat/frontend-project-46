e31c545365c690fe40c79e6e170b6299cce5492f
Stepan Kormilin <ubscoder@gmail.com>
2025-01-16 17:30:22 +0300

    rework after review

--- src/genDiffTree.js
+++ src/genDiffTree.js
@@ -30,8 +30,8 @@ const genDiffTree = (obj1, obj2) => {
     if (obj1[key] === obj2[key]) {
       return { status: 'unchanged', key, value: value1 };
     }
-
-    return { status: 'modified', key, value1, value2 };
+    
+    return { status: 'modified', key, value: [value1, value2] };
   });
 };
 
